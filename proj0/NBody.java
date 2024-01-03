1086eb28aafc98201240a4d7f505b23c1a8e9bda
Akazard <magsheep@outlook.com>
2024-01-03 09:31:14 +0800

    readDATA

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,7 +1,31 @@
+import java.util.Scanner;
+
 public class NBody {
     public static double readRadius(String s) {
         In in = new In(s);
         in.readInt();
         return in.readDouble();
+    }
+
+    public static Planet[] readPlanets(String s){
+        In in = new In(s);
+        Planet[] Planets = new Planet[in.readInt()];
+        in.readDouble();
+        for(int i = 0; i < Planets.length; i++){
+            Planet planet = new Planet(in.readDouble(), in.readDouble(), in.readDouble(), in.readDouble(), in.readDouble(), in.readString());
+            Planets[i] = planet;
+        }
+        return Planets;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double T = Double.parseDouble(scanner.next());
+        double dt = Double.parseDouble(scanner.next());
+        String filename = scanner.next();
+        double radius = readRadius(filename);
+        Planet[] Planets = readPlanets(filename);
+        
+        scanner.close();
     }
 }
